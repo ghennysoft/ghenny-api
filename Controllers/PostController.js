a41aec8959e4dee4ee544ae77d227cb2b385cca0
GhennySoft <ghennysoft@gmail.com>
2024-02-19 21:58:40 +0100

    CRUD Post

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -1,7 +1,52 @@
+import PostModel from "../Models/postModel.js"
 
-export const    getPosts = async (req, res) => {
+export const createPost = async (req, res) => {
     try {
-        res.status(200).json('Getting Posts')
+        const newPost = new PostModel(req.body);
+        await newPost.save();
+        res.status(200).json(newPost)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+export const getPost = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const post = await PostModel.findById(id)
+        res.status(200).json(post)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+export const updatePost = async (req, res) => {
+    const postId = req.params.id;
+    const {currentUserId} = req.body;
+    try {
+        const post = await PostModel.findById(postId)
+        if(post.userId === currentUserId) {
+            await post.updateOne({$set:req.body})
+            res.status(200).json('Post Updated!')
+        } else {
+            res.status(403).json('Action Forbidden')
+        }
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+export const deletePost = async (req, res) => {
+    const postId = req.params.id;
+    const {currentUserId} = req.body;
+    try {
+        const post = await PostModel.findById(postId)
+        if(post.userId === currentUserId) {
+            await post.deleteOne();
+            res.status(200).json('Post Deleted!')
+        } else {
+            res.status(403).json('Action Forbidden')
+        }
     } catch (error) {
         res.status(500).json(error)
     }
--- Routes/PostRoute.js
+++ Routes/PostRoute.js
@@ -1,8 +1,11 @@
 import express from 'express'
-import { getPosts } from '../Controllers/PostController.js'
+import { createPost, deletePost, getPost, updatePost } from '../Controllers/PostController.js'
 
 const router = express.Router()
 
-router.get('/', getPosts)
+router.post('/create', createPost)
+router.get('/:id', getPost)
+router.put('/:id', updatePost)
+router.delete('/:id', deletePost)
 
 export default router
