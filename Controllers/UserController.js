92882064a2e87664185c5a1865bfc00d9f9dd3be
GhennySoft <ghennysoft@gmail.com>
2024-03-02 09:44:35 +0100

    Verify Token

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,16 +1,16 @@
 import UserModel from "../Models/userModel.js";
+import { createError } from "../error.js";
 
 
 // Get a User
-export const getUser = async (req, res) => {
-    const id = req.params.id;
+export const getUser = async (req, res, next) => {
+    const paramId = req.params.id;
 
     try {
-        const user = await UserModel.findById(id);
-
+        const user = await UserModel.findById(paramId);
         if(user){
-            const {password, ...otherDetails} = user._doc
-            res.status(200).json(otherDetails)
+            const {password, ...other} = user._doc
+            res.status(200).json(other)
         }else{
             res.status(404).json("No such user exist")
         }
@@ -22,36 +22,36 @@ export const getUser = async (req, res) => {
 
 
 // Update User
-export const updateUser = async (req, res) => {
-    const id = req.params.id;
-    const {currentUserId} = req.body;
+export const updateUser = async (req, res, next) => {
+    const paramId = req.params.id;
+    const currentUserId = req.user.id;
 
-    if(id === currentUserId) {
-        try {
-            const user = await UserModel.findByIdAndUpdate(id, req.body, {new:true});
+    if(paramId === currentUserId) {
+        try { 
+            const user = await UserModel.findByIdAndUpdate(paramId, {$set: req.body}, {new:true});
             res.status(201).json(user)
         } catch (error) {
             res.status(500).json(error)
         }
     } else {
-        res.status(403).json("Access Denied, you can only update your profile!")
+        return next(createError(403, "Access Denied, you can only update your profile!"))
     }
 }
 
 
 // Delete User
-export const deleteUser = async (req, res) => {
-    const id = req.params.id;
-    const {currentUserId} = req.body;
+export const deleteUser = async (req, res, next) => {
+    const paramId = req.params.id;
+    const currentUserId = req.user.id;
 
-    if(id === currentUserId) {
-        try {
-            await UserModel.findByIdAndDelete(id);
-            res.status(201).json("User Deleted Successfully")
-        } catch (error) {
-            res.status(500).json(error)
+    if(paramId === currentUserId) {
+        try { 
+            const user = await UserModel.findByIdAndDelete(paramId);
+            res.status(201).json("User deleted successfully")
+        } catch (err) {
+            next(err);
         }
     } else {
-        res.status(403).json("Access Denied, you can only delete your profile!")
+        return next(createError(403, "Access Denied, you can only delete your profile!"))
     }
 }
--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { deleteUser, getUser, updateUser } from '../Controllers/UserController.js'
+import { verifyToken } from '../verifyToken.js'
 
 const router = express.Router()
 
 router.get('/:id', getUser)
-router.put('/:id', updateUser)
-router.delete('/:id', deleteUser)
+router.put('/:id', verifyToken, updateUser)
+router.delete('/:id', verifyToken, deleteUser)
 
 export default router
